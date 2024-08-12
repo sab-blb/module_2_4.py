727fd149320589f7516d9aa81355628549630a81
Alexandr <alexandr_balabanov55@mail.ru>
2024-08-13 00:37:07 +0500

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -3,14 +3,16 @@ print(f"{"Задача"} {'"Всё не так уж просто"'}")
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-is_prime = True
-for i in numbers[0::]:
-    for j in numbers[0::]:
-        if i >= 2 and j >= 2:
-            if i % j == 0 and i / j == 1:
-                primes.append(i)
-            elif i % j == 0 and i / j != 1:
-                not_primes.append(i)
-                break
+for i, j in enumerate(numbers):
+    if j < 2:
+        continue
+    is_prime = True
+    for div in range(2, j):
+        if j % div == 0:
+            not_primes.append(j)
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(j)
 print("Primes:", primes)
 print("Not Primes:", not_primes)
